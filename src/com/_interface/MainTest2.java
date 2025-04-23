e748303a70471c3c4ceb98e9b3fc741539984a62
dhdhfkk1119 <whwhwjddn123@gmail.com>
2025-04-23 15:49:08 +0900

    인터페이스 기본 문법 확인

---
+++ src/com/_interface/MainTest2.java
@@ -0,0 +1,11 @@
+package com._interface;
+
+public class MainTest2 {
+    public static void main(String[] args) {
+        Calc cale = new CompleteCalc();
+        cale.add(10, 20);
+        cale.add(20, 30);
+        cale.add(30, 40);
+        cale.add(40, 50);
+    }
+}
